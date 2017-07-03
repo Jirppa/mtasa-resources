879937f047ec329e7235d3cb939cc3f14f9ec828
Jirppa <Jirppa@haxaa.net>
2017-07-03 13:45:39 +0300

    Cleaning script and improving performance
    - Made tasks in table to be keys instead of values for faster and cleaner way to access them
    - Added some checks so player cant fire reload event if they do not have a proper weapon to reload
    - Using latent event instead of normal event to reduce cpu usage and network traffic
    - Made the command and bindKey with a bit cleaner way
    - Used "reload" command instead "Reload weapon" for increased player-friendliness

--- [gameplay]/reload/reload_c.lua
+++ [gameplay]/reload/reload_c.lua
@@ -1,38 +1,38 @@
-﻿local blockedTasks = 
+local blockedTasks = 
 {
-	"TASK_SIMPLE_IN_AIR", -- We're falling or in a jump.
-	"TASK_SIMPLE_JUMP", -- We're beginning a jump
-	"TASK_SIMPLE_LAND", -- We're landing from a jump
-	"TASK_SIMPLE_GO_TO_POINT", -- In MTA, this is the player probably walking to a car to enter it
-	"TASK_SIMPLE_NAMED_ANIM", -- We're performing a setPedAnimation
-	"TASK_SIMPLE_CAR_OPEN_DOOR_FROM_OUTSIDE", -- Opening a car door
-	"TASK_SIMPLE_CAR_GET_IN", -- Entering a car
-	"TASK_SIMPLE_CLIMB", -- We're climbing or holding on to something
-	"TASK_SIMPLE_SWIM",
-	"TASK_SIMPLE_HIT_HEAD", -- When we try to jump but something hits us on the head
-	"TASK_SIMPLE_FALL", -- We fell
-	"TASK_SIMPLE_GET_UP" -- We're getting up from a fall
+	["TASK_SIMPLE_IN_AIR"] = true, -- We're falling or in a jump.
+	["TASK_SIMPLE_JUMP"] = true, -- We're beginning a jump
+	["TASK_SIMPLE_LAND"] = true, -- We're landing from a jump
+	["TASK_SIMPLE_GO_TO_POINT"] = true, -- In MTA, this is the player probably walking to a car to enter it
+	["TASK_SIMPLE_NAMED_ANIM"] = true, -- We're performing a setPedAnimation
+	["TASK_SIMPLE_CAR_OPEN_DOOR_FROM_OUTSIDE"] = true, -- Opening a car door
+	["TASK_SIMPLE_CAR_GET_IN"] = true, -- Entering a car
+	["TASK_SIMPLE_CLIMB"] = true, -- We're climbing or holding on to something
+	["TASK_SIMPLE_SWIM"] = true,
+	["TASK_SIMPLE_HIT_HEAD"] = true, -- When we try to jump but something hits us on the head
+	["TASK_SIMPLE_FALL"] = true, -- We fell
+	["TASK_SIMPLE_GET_UP"] = true -- We're getting up from a fall
 }
 
 local function reloadWeapon()
-	-- Usually, getting the simplest task is enough to suffice
-	local task = getPedSimplestTask (localPlayer) 
 
-	-- Iterate through our list of blocked tasks
-	for idx, badTask in ipairs(blockedTasks) do
-		-- If the player is performing any unwanted tasks, do not fire an event to reload
-		if (task == badTask) then
-			return
-		end
-	end
+	local task = getPedSimplestTask (localPlayer) -- Usually, getting the simplest task is enough to suffice
+	local slot = getPedWeaponSlot(localPlayer)
 	
-	triggerServerEvent("relWep", resourceRoot)
+	if blockedTasks[task] then return end --If the player is performing any unwanted tasks, do not fire an event to reload
+	if slot < 2 or slot > 5 then return end --If the player does not have a reloadable gun, do not fire an event to reload
+	
+	triggerLatentServerEvent("relWep",4000,false,resourceRoot) --Its really small event so use latent event instead to reduce traffic and CPU usage
+	
 end
 
 -- The jump task is not instantly detectable and bindKey works quicker than getControlState
 -- If you try to reload and jump at the same time, you will be able to instant reload.
 -- We work around this by adding an unnoticable delay to foil this exploit.
-addCommandHandler("Reload weapon", function()
-	setTimer(reloadWeapon, 50, 1)
-end)
-bindKey("r", "down", "Reload weapon")
+
+local function reload()
+	setTimer(reloadWeapon,50,1)
+end
+
+addCommandHandler("reload",reload,false)
+bindKey("r","down","reload")
